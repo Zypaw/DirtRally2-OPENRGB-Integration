49acc56bfccda68dd339a627fde13e1d3946a4f3
zypaw <zypawytb@gmail.com>
2024-02-09 18:14:44 +0100

    save

--- openRGB_handler.py
+++ openRGB_handler.py
@@ -2,12 +2,18 @@ from openrgb import OpenRGBClient
 from openrgb.utils import RGBColor, ZoneType
 
 cli = OpenRGBClient()
+cli.clear()
 
+led = cli.get_devices_by_name("WLED")[0]
+
+while True:
+    for i in range(10):
+        led.zones[0].leds[i].set_color(RGBColor(255,0,0),fast=True)
 # finding a linear zone
-zone = next(z for dev in cli.ee_devices for z in dev.zones if z.type == ZoneType.LINEAR)
-# dividing the color spectrum by number of LEDs in the zone
-step = int(360/len(zone.colors))
-# Setting the zones colors to the entire spectrum of the rainbow
-for i, hue in enumerate(range(0, 360, step)):
-    zone.colors[i] = RGBColor.fromHSV(hue, 100, 100)
-zone.show()
+# zone = next(z for dev in cli.ee_devices for z in dev.zones if z.type == ZoneType.LINEAR)
+# # dividing the color spectrum by number of LEDs in the zone
+# step = int(360/len(zone.colors))
+# # Setting the zones colors to the entire spectrum of the rainbow
+# for i, hue in enumerate(range(0, 360, step)):
+#     zone.colors[i] = RGBColor.fromHSV(hue, 100, 100)
+# zone.show()
--- telemetry.py
+++ telemetry.py
@@ -1,7 +1,7 @@
 import socket, struct, time
 # port & ip
-HOST='0.0.0.0'
-PORT=20777
+HOST='localhost'
+PORT=10001
 # gear map
 
 GEAR={0.:'N', 1.:'1', 2.:'2', 3.:'3', 4.:'4', 5.:'5', 6.:'6', 7.:'7', 8.:'8', 9.:'9', 10.:'R'}
@@ -12,7 +12,15 @@ POS_RPM_CURR=37
 POS_V=7
 
 RETRY_MAX=15
-BUFF_SIZE=512
+BUFF_SIZE=264
+
+
+from openrgb import OpenRGBClient
+from openrgb.utils import RGBColor, ZoneType
+
+cli = OpenRGBClient()
+cli.clear()
+led = cli.get_devices_by_name("WLED")[0]
 
 
 for i in range(RETRY_MAX):
@@ -25,18 +33,42 @@ for i in range(RETRY_MAX):
         # read loop
         while True:
             b = sock.recv(BUFF_SIZE)
-
             if b:
-                print(len(b))
                 # receieved, process em'
                 info = struct.unpack('64f', b[:256])
                 try:
-                    print(f"GEAR : {GEAR[info[POS_GEAR]]}, POWER : {info[POS_RPM_CURR]*100./info[POS_RPM_MAX]}, RPMS : {info[POS_RPM_CURR]} ,MAX_RPMS : {info[POS_RPM_MAX]}, SPEED : {info[POS_V]*3.6}")
-                except:
-                    print(f"GEAR : {GEAR[info[POS_GEAR]]}, POWER : {info[POS_RPM_CURR]*100./info[POS_RPM_MAX]}, RPMS : {info[POS_RPM_CURR]} ,MAX_RPMS : {info[POS_RPM_MAX]}, SPEED : {info[POS_V]*3.6}")
+                    ratio = (6.5/8)
+                    if info[POS_RPM_CURR] >= ratio*(info[POS_RPM_MAX]-50) and POS_RPM_CURR != 0:
+                        count = (info[POS_RPM_CURR]-ratio*info[POS_RPM_MAX])/(info[POS_RPM_MAX]-ratio*info[POS_RPM_MAX])*17
+                        led.zones[0].set_color(RGBColor(0,0,0),fast=True)
+                        if int(count) > 15:
+                            count = 15.01
+                        if int(count) > 2:
+                            green = [RGBColor(0, 255, 0)] * 2
+                        else:
+                            green = [RGBColor(0, 0, 0)]
+                        if int(count) > 5:
+                            yellow = [RGBColor(180, 180, 0)] * 5
+                        else:
+                            if int(count) > 2:
+                                yellow = [RGBColor(180, 180, 0)] * int(count)
+                            else:
+                                yellow = [RGBColor(0, 0, 0)]
+                        if int(count) > 7:
+                            red = [RGBColor(255, 0, 0)] * (int(count)-7)
+                        else:
+                            red = [RGBColor(0, 0, 0)]
+                        remaining_leds = 15 - len(red) - len(yellow) - len(green)
+                        led.zones[0].set_colors(green + yellow + red + [RGBColor(0, 0, 0)] * (remaining_leds*2)+ red + yellow + green,fast=True)
+                    else:
+                        led.zones[0].set_color(RGBColor(0,0,0),fast=True)
+                    print(f"GEAR : {GEAR[info[POS_GEAR]]}, POWER : {int(info[POS_RPM_CURR]*100./info[POS_RPM_MAX])}, RPMS : {int(info[POS_RPM_CURR])} ,MAX_RPMS : {int(info[POS_RPM_MAX])}, SPEED : {info[POS_V]*3.6}")
+                except Exception as E:
+                    print(E)
             else:
                 # broken sock, fallback and reconnect
                 raise Exception('broken sock')
+            # time.sleep(0.1)
     except Exception as E:
         print(E, ', retry %d'%i)
         time.sleep(1)
